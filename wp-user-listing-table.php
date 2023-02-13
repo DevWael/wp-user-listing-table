fac27745279ed057fbcc9667fa40e418a1eb3b2b
devwael <dev.ahmedwael@gmail.com>
2023-02-13 23:04:39 +0200

    initialize the plugin instance

--- wp-user-listing-table.php
+++ wp-user-listing-table.php
@@ -1,10 +1,28 @@
-<?php
+<?php # -*- coding: utf-8 -*-
 /**
  * Plugin Name: WP User Listing Table
  * Plugin URI: https://github.com/DevWael/wp-user-listing-table
  * Description: WordPress plugin that provide users listing from a remote API on a custom URL endpoint
- * Version: 1.0
+ * Version: 1.0.0
  * Author: Ahmad Wael
  * Author URI: https://www.bbioon.com
  * License: MIT
  */
+
+namespace WpUserListingTable;
+
+if (!function_exists('add_filter')) {
+    return;
+}
+
+add_action('plugins_loaded', __NAMESPACE__.'\initialize');
+function initialize()
+{
+    $autoLoad = plugin_dir_path(__FILE__).'vendor/autoload.php';
+    if (!class_exists(UserListing::class) && is_readable($autoLoad)) {
+        /** @noinspection PhpIncludeInspection */
+        require_once $autoLoad;
+    }
+
+    class_exists(UserListing::class) && UserListing::instance();
+}
