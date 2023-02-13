1b37fd65c28734ff47659908787dd17012e9662b
devwael <dev.ahmedwael@gmail.com>
2023-02-13 18:06:44 +0200

    initial files

---
+++ wp-user-listing-table.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP User Listing Table
+ * Plugin URI: https://github.com/DevWael/wp-user-listing-table
+ * Description: WordPress plugin that provide users listing from a remote API on a custom URL endpoint
+ * Version: 1.0
+ * Author: Ahmad Wael
+ * Author URI: https://www.bbioon.com
+ * License: MIT
+ */
