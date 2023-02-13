2b4c56c9e677d91182e0e89ef60c635706dd6908
devwael <dev.ahmedwael@gmail.com>
2023-02-13 23:05:07 +0200

    build main plugin class

---
+++ src/UserListing.php
@@ -0,0 +1,50 @@
+<?php
+
+// -*- coding: utf-8 -*-
+
+declare(strict_types=1);
+
+namespace WpUserListingTable;
+
+ use WpUserListingTable\Admin\AdminPage;
+
+class UserListing
+{
+    // Hold the class instance.
+    private static $instance = null;
+
+    /**
+     * User_Listing constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return UserListing
+     *
+     */
+    public static function instance(): self
+    {
+        if (self::$instance === null) {
+            $instance = new self();
+            $instance->init();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Initialize the logic
+     */
+    public function init()
+    {
+        if (wp_installing()) {
+            return;
+        }
+        if (is_admin()) {
+            $adminPage = new AdminPage();
+            \add_action('init', [$adminPage, 'init']);
+        }
+    }
+}
