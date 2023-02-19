2119e99e2ee5421612922ed0ce38037ddee5b630
devwael <dev.ahmedwael@gmail.com>
2023-02-19 02:51:28 +0200

    AssetsLoader.php: - add consts to use in enqueuing assets. - add private method pluginResourcesDirUrl to provide frontend assets resources dir

--- src/FrontEnd/Assets/AssetsLoader.php
+++ src/FrontEnd/Assets/AssetsLoader.php
@@ -12,24 +12,14 @@ namespace WpUserListingTable\FrontEnd\Assets;
 class AssetsLoader implements Assets
 {
     /**
-     * @var string $pluginName plugin unique name.
+     * The unique identifier of this plugin.
      */
-    private string $pluginName;
+    private const PLUGIN_NAME = 'wp-user-listing';
 
     /**
-     * @var string $pluginName current plugin version.
+     * The current version of the plugin.
      */
-    private string $pluginVersion;
-
-    /**
-     * @param string $pluginName plugin unique name.
-     * @param string $pluginVersion current plugin version.
-     */
-    public function __construct(string $pluginName, string $pluginVersion)
-    {
-        $this->pluginName = $pluginName;
-        $this->pluginVersion = $pluginVersion;
-    }
+    private const PLUGIN_VERSION = '1.0.0';
 
     /**
      * Load CSS styles files.
@@ -41,9 +31,9 @@ class AssetsLoader implements Assets
          * Check if the plugin frontend page is opened before loading the assets files.
          */
         if (\get_query_var('table_template') === 'user-listing-table') {
-            $handle = $this->pluginName . '-style';
-            $src = PLUGIN_URI . 'resources/css/style.css';
-            \wp_enqueue_style($handle, $src, [], $this->pluginVersion);
+            $handle = self::PLUGIN_NAME . '-style';
+            $src = $this->pluginResourcesDirUrl() . 'css/style.css';
+            \wp_enqueue_style($handle, $src, [], self::PLUGIN_VERSION);
         }
     }
 
@@ -57,9 +47,19 @@ class AssetsLoader implements Assets
          * Check if the plugin frontend page is opened before loading the assets files.
          */
         if (\get_query_var('table_template') === 'user-listing-table') {
-            $handle = $this->pluginName . '-script';
-            $src = PLUGIN_URI . 'resources/js/main.js';
-            \wp_enqueue_script($handle, $src, ['jquery'], $this->pluginVersion, true);
+            $handle = self::PLUGIN_NAME . '-script';
+            $src = $this->pluginResourcesDirUrl() . 'js/main.js';
+            \wp_enqueue_script($handle, $src, ['jquery'], self::PLUGIN_VERSION, true);
         }
+    }
+
+    /**
+     * Prepare the resources URL to the resources plugin directory.
+     *
+     * @return string
+     */
+    private function pluginResourcesDirUrl(): string
+    {
+        return plugin_dir_url(dirname(__FILE__, 3)) . 'resources/';
     }
 }
