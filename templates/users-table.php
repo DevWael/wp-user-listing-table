f576058d923badbcc7d7d9d7a86f48f04e5ec28b
devwael <dev.ahmedwael@gmail.com>
2023-02-22 04:48:02 +0200

    build the frontend html

--- templates/users-table.php
+++ templates/users-table.php
@@ -12,9 +12,54 @@
 defined('ABSPATH') || exit;
 get_header();
 do_action('wp_user_table_start');
+$usersProvider = new WpUserListingTable\FrontEnd\Data\UsersProvider();
+$users = apply_filters('wp_user_table_users_list', $usersProvider->usersList());
 ?>
-    <div id="main" class="wp-users-table-template">
-        Main template goes here...
+    <div class="wp-users-table-template">
+        <?php
+        if ( ! empty($users)): ?>
+            <div class="table-wrapper">
+                <table id="usersTable"
+                       class="wp-users-table wp-users-table-js has-background">
+                    <thead>
+                    <tr>
+                        <th><?php
+                            esc_html_e('ID', 'wp-user-listing') ?></th>
+                        <th><?php
+                            esc_html_e('Name', 'wp-user-listing') ?></th>
+                        <th><?php
+                            esc_html_e('Username', 'wp-user-listing') ?></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($users
+
+                    as $user):
+                    $userID = $user['id'] ?? '';
+                    $name = $user['name'] ?? '';
+                    $userName = $user['username'] ?? '';
+                    ?>
+                    <tr>
+                        <td data-user-id="<?php
+                        echo esc_attr($userID) ?>"><?php
+                            echo esc_html($userID) ?></td>
+                        <td data-user-id="<?php
+                        echo esc_attr($userID) ?>"><?php
+                            echo esc_html($name) ?></td>
+                        <td data-user-id="<?php
+                        echo esc_attr($userID) ?>"><?php
+                            echo esc_html($userName) ?></td>
+                        <?php
+                        endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+
+        <?php
+        endif; ?>
+        <div class="overlay hidden"></div>
+        <div class="wp-single-user-popup-container"></div>
     </div>
 <?php
 do_action('wp_user_table_end');
