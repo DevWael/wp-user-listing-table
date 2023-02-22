c409100c1385a1ee2e97702f1b15f55bc0838cf3
devwael <dev.ahmedwael@gmail.com>
2023-02-22 02:30:15 +0200

    Add the object that will be used for the ajax and for localizing frontend components.

--- src/FrontEnd/Assets/AssetsLoader.php
+++ src/FrontEnd/Assets/AssetsLoader.php
@@ -23,6 +23,7 @@ class AssetsLoader implements Assets
 
     /**
      * Load CSS styles files.
+     *
      * @return void
      */
     public function loadCSS(): void
@@ -40,6 +41,7 @@ class AssetsLoader implements Assets
 
     /**
      * Load Javascript files.
+     *
      * @return void
      */
     public function loadJS(): void
@@ -50,7 +52,48 @@ class AssetsLoader implements Assets
         if (\get_query_var('table_template') === 'user-listing-table') {
             $handle = self::PLUGIN_NAME . '-script';
             $src = $this->pluginResourcesDirUrl() . 'js/main.js';
-            \wp_enqueue_script($handle, $src, ['jquery'], self::PLUGIN_VERSION, true);
+            \wp_enqueue_script(
+                $handle,
+                $src,
+                ['jquery'],
+                self::PLUGIN_VERSION,
+                true
+            );
+            /**
+             * Add the object that will be used for the ajax and for localizing
+             * frontend components.
+             */
+            \wp_localize_script($handle, 'usersTableObject', [
+                'ajaxURL' => esc_url(admin_url('admin-ajax.php')),
+                'nonce' => wp_create_nonce(),
+                'action' => 'users_table_request',
+                'i18n' => [
+                    'popupTitle' => esc_html__(
+                        'User Information',
+                        'wp-user-listing'
+                    ),
+                    'id' => esc_html__('ID', 'wp-user-listing'),
+                    'name' => esc_html__('Name', 'wp-user-listing'),
+                    'username' => esc_html__('Username', 'wp-user-listing'),
+                    'email' => esc_html__('Email', 'wp-user-listing'),
+                    'phone' => esc_html__('Phone', 'wp-user-listing'),
+                    'address' => esc_html__('Address', 'wp-user-listing'),
+                    'street' => esc_html__('Street', 'wp-user-listing'),
+                    'suite' => esc_html__('Suite', 'wp-user-listing'),
+                    'city' => esc_html__('City', 'wp-user-listing'),
+                    'zip' => esc_html__('Zip Code', 'wp-user-listing'),
+                    'lat' => esc_html__('Latitude', 'wp-user-listing'),
+                    'lng' => esc_html__('Longitude', 'wp-user-listing'),
+                    'website' => esc_html__('Website', 'wp-user-listing'),
+                    'company' => esc_html__('Company', 'wp-user-listing'),
+                    'companyName' => esc_html__('Name', 'wp-user-listing'),
+                    'catchphrase' => esc_html__(
+                        'Catchphrase',
+                        'wp-user-listing'
+                    ),
+                    'business' => esc_html__('Business', 'wp-user-listing'),
+                ],
+            ]);
             \do_action('wp_users_table_load_js_assets');
         }
     }
@@ -67,6 +110,7 @@ class AssetsLoader implements Assets
 
     /**
      * Attach the class functions to WordPress hooks
+     *
      * @return void
      */
     public function init(): void
