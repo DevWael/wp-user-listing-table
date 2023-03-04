69c2d6b5c50ad5ef6dbf787d2368fd7814f8aaf1
devwael <dev.ahmedwael@gmail.com>
2023-03-05 01:19:53 +0200

    add LanguagesTest.php test cases

---
+++ tests/PHPUnit/Unit/I18n/LanguagesTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PHPUnit\Unit\I18n;
+
+use WpUserListingTable\I18n\Languages;
+use WpUserListingTable\PHPUnit\Unit\AbstractUnitTestCase;
+
+class LanguagesTest extends AbstractUnitTestCase
+{
+    /**
+     * Test loading languages.
+     *
+     * @return void
+     */
+    public function testLoadTextDomain(): void
+    {
+        \WP_Mock::userFunction('plugin_basename')->andReturn('plugin_basename');
+        \WP_Mock::userFunction('load_plugin_textdomain');
+        try {
+            $languages = new Languages();
+            $languages->loadTextDomain();
+        } catch (\InvalidArgumentException $notExpected) {
+            $this->fail();
+        }
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Test if the init method contains required hooks
+     *
+     * @return void
+     */
+    public function testinit(): void
+    {
+        $languages = new Languages();
+        \WP_Mock::expectActionAdded('plugins_loaded',[$languages,'loadTextDomain']);
+        $languages->init();
+        \WP_Mock::assertActionsCalled();
+    }
+}
