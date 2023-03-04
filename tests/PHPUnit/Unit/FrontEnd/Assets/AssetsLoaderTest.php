3b09e634063a49e92da9181bb19b00155c75f226
devwael <dev.ahmedwael@gmail.com>
2023-03-05 01:45:20 +0200

    add assets loader class unit tests

---
+++ tests/PHPUnit/Unit/FrontEnd/Assets/AssetsLoaderTest.php
@@ -0,0 +1,74 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace PHPUnit\Unit\FrontEnd\Assets;
+
+use WpUserListingTable\FrontEnd\Assets\AssetsLoader;
+use WpUserListingTable\PHPUnit\Unit\AbstractUnitTestCase;
+
+class AssetsLoaderTest extends AbstractUnitTestCase
+{
+    /**
+     * Test if load js method works when query var equals user-listing-table
+     *
+     * @return void
+     */
+    public function testLoadJSWorksIfQueryVarEqualsString()
+    {
+        \WP_Mock::userFunction('get_query_var', [
+            'args' => 'table_template',
+            'return' => 'user-listing-table',
+        ]);
+        \WP_Mock::userFunction('plugin_dir_url', [
+            'return' => '/',
+        ]);
+        \WP_Mock::userFunction('wp_enqueue_script');
+        \WP_Mock::userFunction('wp_localize_script');
+        \WP_Mock::userFunction('admin_url');
+        \WP_Mock::userFunction('wp_create_nonce');
+        \WP_Mock::expectAction('wp_users_table_load_js_assets');
+        $assetsLoader = new AssetsLoader();
+        $assetsLoader->loadJS();
+        \WP_Mock::assertActionsCalled();
+    }
+
+    /**
+     * Test if load css method works when query var equals user-listing-table
+     *
+     * @return void
+     */
+    public function testLoadCSSWorksIfQueryVarEqualsString(): void
+    {
+        \WP_Mock::userFunction('get_query_var', [
+            'args' => 'table_template',
+            'return' => 'user-listing-table',
+        ]);
+        \WP_Mock::userFunction('plugin_dir_url', [
+            'return' => '/',
+        ]);
+        \WP_Mock::userFunction('wp_enqueue_style');
+        \WP_Mock::expectAction('wp_users_table_load_css_assets');
+        $assetsLoader = new AssetsLoader();
+        $assetsLoader->loadCSS();
+        \WP_Mock::assertActionsCalled();
+    }
+
+    /**
+     * Test if the init method contains required hooks
+     *
+     * @return void
+     */
+    public function testInitMethod(): void
+    {
+        $assetsLoader = new AssetsLoader();
+        \WP_Mock::expectActionAdded(
+            'wp_enqueue_scripts',
+            [$assetsLoader, 'loadCSS']
+        );
+        \WP_Mock::expectActionAdded(
+            'wp_enqueue_scripts',
+            [$assetsLoader, 'loadJS']
+        );
+        $assetsLoader->init();
+        \WP_Mock::assertActionsCalled();
+    }
+}
