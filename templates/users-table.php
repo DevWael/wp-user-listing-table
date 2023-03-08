72ae771fc99ffed29d3f5e41f1535435abf62d40
devwael <dev.ahmedwael@gmail.com>
2023-03-08 05:31:28 +0200

    Load header and footer from Components class

--- templates/users-table.php
+++ templates/users-table.php
@@ -11,41 +11,19 @@
 
 defined('ABSPATH') || exit;
 
+use WpUserListingTable\FrontEnd\Data\UsersProvider;
+use WpUserListingTable\FrontEnd\Routes\Templates\Components;
+
 /**
- * Check if the installed theme is block theme (FSE Themes).
- * If so, then we won't load the get_header() function as the theme doesn't
- * include any header.php file.
- * We will load the theme header block instead.
- *
- * @see https://core.trac.wordpress.org/ticket/55023
+ * Load the header component.
  */
-$blockTheme = wp_is_block_theme();
-if ($blockTheme) {
-    ?><!doctype html>
-    <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <div class="wp-site-blocks">
-        <header class="wp-block-template-part">
-            <?php block_header_area() ?>
-        </header>
-    <?php
-}
-/**
- * If it's traditional WP theme, then load the header.
- */
-if (!$blockTheme) {
-    get_header();
-}
+Components::header();
+
 /**
  * Use this action to add content above the users table.
  */
 do_action('wp_users_table_before_table');
-$usersProvider = new WpUserListingTable\FrontEnd\Data\UsersProvider();
+$usersProvider = new UsersProvider();
 $users = apply_filters('wp_users_table_users_list_template', $usersProvider->usersList());
 ?>
     <div class="wp-users-table-template">
@@ -99,22 +77,6 @@ $users = apply_filters('wp_users_table_users_list_template', $usersProvider->use
 do_action('wp_users_table_after_table');
 
 /**
- * Check if the installed theme is block theme (FSE Themes).
- * If so, then we won't load the get_footer() function as the theme doesn't
- * include any footer.php file.
- * We will load the theme footer block instead.
+ * Load the footer component.
  */
-if ($blockTheme) {
-    ?>
-    <?php block_footer_area(); ?>
-    </div>
-    <?php wp_footer(); ?>
-    </body>
-    </html><?php
-}
-/**
- * If it's traditional WP theme, then load the footer.
- */
-if (!$blockTheme) {
-    get_footer();
-}
+Components::footer();
